6d3bc474bee2515e92b03d584ae4a5ecbe31a0f7
anaerovane <anaerovane@hotmail.com>
2024-11-20 00:10:17 +0800

    112001 revise

--- README.md
+++ README.md
@@ -25,17 +25,27 @@ python main.py --yaml poisson.yaml
 poisson.yaml file need to be created as follows
 
 ```yaml
-image_path: 'E:/mbnet/sh/18.tiff'
-output_directory: 'E:/mbnet/sh/18_image'
-output_label_directory: 'E:/mbnet/sh/18_label'
-output_labelnew_directory: 'E:/mbnet/sh/18_labelnew'
-jpg_directory: 'E:/mbnet/sh/18_jpg'
-output_image_path: 'E:/mbnet/sh/18new.tiff'
-output_jsonl_path: 'E:/mbnet/sh/18new.jsonl'
+image_path: 'E:/mbnet/gaode/xm/18.tiff'
+output_directory: 'E:/mbnet/gaode/xm/18_image'
+output_label_directory: 'E:/mbnet/gaode/xm/18_label'
+output_labelnew_path: 'E:/mbnet/gaode/xm/18_labelnew'  #dir or file ending with .txt are both ok
+jpg_directory: 'E:/mbnet/gaode/xm/18_jpg'
+output_image_path: 'E:/mbnet/gaode/xm/18new.tiff'
+output_jsonl_path: 'E:/mbnet/gaode/xm/18new.jsonl'
 sample_radius: 500
+threshold: 0.6
+cropwidth: 640
+cropheight: 640
+
 ```
 
 We conducted the tests on both Windows CPU systems and Ubuntu 22.04LTS GPU systems (with an Nvidia Tesla M40 GPU VRAM12G)
+
+### Note
+
+We are still making significant modifications and optimizations to the project, including the paper and code!
+
+Please use the project code with caution until we confirm its completion!
 
 ### Contact
 
---
+++ retinanet/retinanet_detect_gpu.py
@@ -0,0 +1,51 @@
+import torch
+from PIL import Image
+from torchvision.models.detection.ssdlite import ssdlite320_mobilenet_v3_large
+from torchvision.models.detection.ssdlite import SSDLite320_MobileNet_V3_Large_Weights
+from torchvision.transforms import functional as F
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(weights='DEFAULT')
+model= torchvision.models.detection.retinanet_resnet50_fpn_v2(weights='DEFAULT')
+#model = ssdlite320_mobilenet_v3_large(weights=SSDLite320_MobileNet_V3_Large_Weights.DEFAULT)
+model.load_state_dict(torch.load('GPU10retinanet.pth'))
+model.to(device)
+model.eval()  
+
+def preprocess_image(image_path):
+    image = Image.open(image_path).convert("RGB")
+    image = F.resize(image, (320, 320))
+    image = F.to_tensor(image)
+    return image.unsqueeze(0).to(device)  
+
+def plot_prediction(image, prediction, threshold=0.3):
+    fig, ax = plt.subplots(1)
+    ax.imshow(image)
+
+    boxes = prediction['boxes'].cpu().numpy()
+    labels = prediction['labels'].cpu().numpy()
+    scores = prediction['scores'].cpu().numpy()
+    
+    for box, label, score in zip(boxes, labels, scores):
+        if score > threshold:  
+            print(box,label,score)
+            rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+            ax.text(box[0], box[1], str(label), color='white', verticalalignment='top', bbox={'color': 'red', 'pad': 0})
+    plt.show()
+
+image_path = '/mnt/data1/1110/data1116/images/train/5_6.tif'
+input_image = preprocess_image(image_path)
+
+with torch.no_grad():
+    predictions = model(input_image)
+
+prediction = predictions[0]
+
+original_image = Image.open(image_path).convert("RGB")
+original_image = original_image.resize((320, 320))
+plot_prediction(original_image, prediction)
+
